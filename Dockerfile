b8af791f3fb406c330379598b04eadb391f3143e
muhammad <muhammad-11@fst.unair.ac.id>
2020-01-20 05:43:17 +0700

    update dockerfile

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,7 @@ RUN add-apt-repository ppa:alex-p/tesseract-ocr
 RUN apt-get update
 RUN apt install -y tesseract-ocr
 RUN apt install -y tesseract-ocr-ind
+RUN apt-get install -y libsm6 libxext6 libxrender-dev
 
 RUN pip install -r requirements.txt
 WORKDIR /app 
--- main.py
+++ main.py
@@ -2,7 +2,13 @@ try:
     from PIL import Image
 except ImportError:
     import Image
+import cv2
 import pytesseract
 
-print(pytesseract.image_to_string(Image.open('image.jfif')).encode('utf-8'))
+img = cv2.imread("image.jfif")
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+_, thres = cv2.threshold(gray, 127, 255, cv2.THRESH_TRUNC)
+
+print(pytesseract.image_to_string(thres, lang="ind").encode('utf-8'))
 
